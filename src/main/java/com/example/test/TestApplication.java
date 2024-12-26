bb0e25d0c57d8c2c1e2fd37e651aabec37d28dcd
GODU-LZR <2980560533@qq.com>
2024-12-27 02:49:18 +0800

    初始化环境

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -1,11 +1,11 @@
 package com.example.test;
 
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan(basePackages = "com.example.test.demos.mappers")
+
 @SpringBootApplication
 public class TestApplication {
 
